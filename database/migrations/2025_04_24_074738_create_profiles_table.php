8a1488550b6363b685e51e59a779395dc5e1d4a0
Al Fatih Muzakki <malfazakki@gmail.com>
2025-04-24 16:24:22 +0700

    feat: complete migration for database schema

---
+++ database/migrations/2025_04_24_074738_create_profiles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reader_id')->constrained()->onDelete('cascade');
+            $table->string('favorite_genre')->nullable();
+            $table->text('bio')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
